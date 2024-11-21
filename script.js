4b98c9cc18d2d8714f86fb0369bd3cb51c8c44ee
Your Name <you@example.com>
2024-11-21 11:24:01 +0100

    2nd

--- script.js
+++ script.js
@@ -13,10 +13,32 @@ function clearDisplay() {
 
 function calculate() {
     const display = document.getElementById('display');
-    try {
-        // Evaluate the expression safely
-        display.innerText = Function('return ' + display.innerText)();
-    } catch (e) {
+    const expression = display.innerText;
+    
+    let isValid = true;
+    let validCharacters = '0123456789+-*/().MathlogcosintanPI';
+    
+    for (let i = 0; i < expression.length; i++) {
+        if (validCharacters.indexOf(expression[i]) === -1) {
+            isValid = false;
+        }
+    }
+
+    if (expression.includes('//')) {
+        isValid = false;
+    } else if (expression.includes('**')) {
+        isValid = false;
+    } else if (expression.includes('++')) {
+        isValid = false;
+    } else if (expression.includes('--')) {
+        isValid = false;
+    } else if (expression.includes('..')) {
+        isValid = false;
+    }
+
+    if (isValid) {
+        display.innerText = new Function('return ' + expression)();
+    } else {
         display.innerText = 'Error';
     }
 }
